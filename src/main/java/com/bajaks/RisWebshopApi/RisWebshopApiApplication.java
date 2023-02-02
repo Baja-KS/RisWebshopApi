20231e2033ba768bc2d39097742e13ffa5b8de1d
Baja-KS <lukic.l210@gmail.com>
2023-02-02 07:36:00 +0100

    swagger

--- src/main/java/com/bajaks/RisWebshopApi/RisWebshopApiApplication.java
+++ src/main/java/com/bajaks/RisWebshopApi/RisWebshopApiApplication.java
@@ -1,10 +1,13 @@
 package com.bajaks.RisWebshopApi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Webshop API", version = "1.0"))
 public class RisWebshopApiApplication {
 
 	public static void main(String[] args) {
